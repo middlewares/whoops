0df8fcac75f5d0f42f02f6e284bddb65af3a5973
oscarotero <oom@oscarotero.com>
2016-12-06 18:23:47 +0100

    updated tests

--- tests/WhoopsTest.php
+++ tests/WhoopsTest.php
@@ -4,20 +4,17 @@ namespace Middlewares\Tests;
 
 use Middlewares\Whoops;
 use Middlewares\Utils\Dispatcher;
-use Middlewares\Utils\CallableMiddleware;
-use Zend\Diactoros\ServerRequest;
-use Zend\Diactoros\Response;
 
 class WhoopsTest extends \PHPUnit_Framework_TestCase
 {
     public function testError()
     {
-        $response = (new Dispatcher([
+        $response = Dispatcher::run([
             new Whoops(),
-            new CallableMiddleware(function () {
+            function () {
                 throw new \Exception('Error Processing Request');
-            }),
-        ]))->dispatch(new ServerRequest());
+            },
+        ]);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
         $this->assertEquals(500, $response->getStatusCode());
@@ -26,12 +23,9 @@ class WhoopsTest extends \PHPUnit_Framework_TestCase
 
     public function testNotError()
     {
-        $response = (new Dispatcher([
+        $response = Dispatcher::run([
             new Whoops(),
-            new CallableMiddleware(function () {
-                return new Response();
-            }),
-        ]))->dispatch(new ServerRequest());
+        ]);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
         $this->assertEquals(200, $response->getStatusCode());
