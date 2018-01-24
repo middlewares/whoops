f1b8463252066495f846fb08f3947250da1f3d37
oscarotero <oom@oscarotero.com>
2018-01-24 21:21:20 +0100

    Code style fix

--- src/Whoops.php
+++ src/Whoops.php
@@ -3,10 +3,10 @@ declare(strict_types = 1);
 
 namespace Middlewares;
 
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Whoops\Handler\JsonResponseHandler;
 use Whoops\Handler\PlainTextHandler;
 use Whoops\Handler\PrettyPageHandler;
